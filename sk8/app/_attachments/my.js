7e50a78b52f74fab6382d89522a510aa94ebfbfe
Michael Bain <mbain@fullsail.edu>
2013-08-02 20:30:41 -0400

    commit 3

--- sk8/app/_attachments/my.js
+++ sk8/app/_attachments/my.js
@@ -1,12 +1,12 @@
 $(document).on("pageinit","#home", function(){
-    $.couch.db("sk8").view("app/skaters", {
+    $.couch.db("sk8").view("app/teams", {
        success: function(data) {
          //console.log(data);
          $('#homeItems').empty();
          $.each(data.rows, function(index, value) {
              var item = (value.value || value.doc);
              $('#homeItems').append($('<li>').append($('<a>')
-                      .attr("href", "skater.html?skater=" + item.origin + item.type + item.sponsor + item.model)
+                      .attr("href", "skater.html?skater=" + item.name)
                       .text(item.name)
              
              )
@@ -17,8 +17,8 @@ $(document).on("pageinit","#home", function(){
    });
 });
 
-var urlVars = function() {
-    var urlData = $(this).data("url");
+var urlVars = function(urlData) {
+    //var urlData = $(this).data("url");
     var urlParts = urlData.split('?');
     var urlPairs = urlParts[1].split('&');
     var urlValues = {};
@@ -30,16 +30,30 @@ var urlVars = function() {
 	 }
 	 return urlValues;
 
-
-
 };
 
-$(document).on("pageinit","#skater", function(){
-  var skater = urlVars()["skater"];
-  console.log(skater);
-  
+$(document).on("pageinit",'#skater',  function(){
+  var skater = urlVars($(this).data("url"));
+	$.couch.db("sk8").view("app/skaters", {
+	   success: function(data) {
+		 //console.log(data);
+		 $('#skaterItems').empty();
+		 $.each(data.rows, function(index, value) {
+			 var item = (value.value || value.doc);
+			 $('#skaterItems').append($('<li>').append($('<a>')
+					  .attr("href", "team.html?skater=" + item.model)
+					  .text(item.name)
 
+			 )
+			); 
+		 });
+		 $('#skaterItems').listview('refresh');
+	   }
+   });
 });
+
+
+
 
 
 
